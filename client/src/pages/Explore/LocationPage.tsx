f93f2a02cec03bf8154895bcb19a86301a59d8b0
celiaburrington <burr.celia@gmail.com>
2025-12-31 13:29:21 -0500

    improve encounter card interactions

--- client/src/pages/Explore/LocationPage.tsx
+++ client/src/pages/Explore/LocationPage.tsx
@@ -1,11 +1,11 @@
 import { Button, Container } from "react-bootstrap";
 import { Location } from "../../types/location.types";
 import { FaLocationArrow } from "react-icons/fa6";
-import { Link, useLocation } from "react-router-dom";
 import EncounterCard from "./components/EncounterCard";
 import { Encounter } from "../../types/encounter.types";
 import { useState } from "react";
 import { randomEncounter } from "../../services/encounterService";
+import { Link } from "react-router-dom";
 
 export default function LocationPage({
   location,
@@ -14,66 +14,61 @@ export default function LocationPage({
   location: Location;
   goExplore: () => void;
 }) {
-  const [disabled, setDisabled] = useState<boolean>(false);
   const [areaEncounter, setAreaEncounter] = useState<Encounter | null>(null);
-  const locationURL = useLocation();
 
+  /**
+   * Gets a random encounter from the current location's encounter list.
+   */
   const getRandomEncounter = async () => {
     try {
       const rand = await randomEncounter(location);
       setAreaEncounter(rand);
-      setDisabled(true);
     } catch (error) {
       console.log(error);
     }
   };
 
+  /**
+   * Reset the encounter state variable.
+   */
   const resetLocationEncounter = () => {
     setAreaEncounter(null);
-    setDisabled(false);
   };
 
   // TODO: Handle blocking user from refreshing the page at same location.
 
   return (
     <Container>
+      <h3 className="mt-2">{location.name}</h3>
+      {location.description}
+      <hr />
+      {!areaEncounter && (
+        <>
+          <button
+            className="border-0 bg-transparent pp-get-encounter"
+            onClick={getRandomEncounter}
+          >
+            <EncounterCard key={0} />
+          </button>
+          <br />
+        </>
+      )}
+      {areaEncounter && (
+        <div className="pp-get-encounter">
+          <EncounterCard key={0} pokemon={areaEncounter.pokemon} />
+        </div>
+      )}
       <Button
-        className="btn-primary"
+        className="btn-primary mt-3"
         onClick={() => {
           goExplore();
           resetLocationEncounter();
         }}
       >
-        Keep Exploring <FaLocationArrow />
+        Keep Exploring <FaLocationArrow className="mb-1" />
       </Button>
-      <h3 className="mt-2">{location.name}</h3>
-      {location.description}
-      <hr />
-      {!areaEncounter && <EncounterCard key={0} />}
-      {areaEncounter && (
-        <EncounterCard key={0} pokemon={areaEncounter.pokemon} />
-      )}
-      <br />
-      {!disabled && (
-        <Button
-          className="btn-primary me-2"
-          onClick={() => getRandomEncounter()}
-        >
-          Encounter!
-        </Button>
-      )}
-      {disabled && areaEncounter && (
-        <Link
-          to={{
-            pathname: `/Details/${areaEncounter.pokemon.api_id}`,
-          }}
-          state={{ from: locationURL }}
-        >
-          <Button className="btn-primary me-2">See Details</Button>
-        </Link>
-      )}
       <Link to="/Home">
-        <Button className="btn-danger">Return Home</Button>
+        <Button className="btn-danger mt-3 ms-1">Return Home</Button>
       </Link>
     </Container>
   );
